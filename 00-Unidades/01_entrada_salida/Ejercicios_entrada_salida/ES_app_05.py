daa21f9cda23394b4ebd52c5f7330d4f79649413
MiguelGodoy1 <Miguel.godoy28@hotmail.com>
2024-02-05 22:19:01 -0300

    Ejercicio S/05

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_05.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_05.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre:Miguel
+apellido:Godoy
 ---
 Ejercicio: entrada_salida_05
 ---
@@ -40,7 +40,9 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        nombre = self.txt_nombre.get()
+        edad = self.txt_edad.get()
+        alert ("informacion","Su nombre es "+ nombre +" y su edad es de "+ edad)
 
         
     
