53d855b01721e5d2528ec849e31c733dd867aad3
MiguelGodoy1 <Miguel.godoy28@hotmail.com>
2024-02-11 15:03:42 -0300

    Tp3

--- 00-Unidades/01_entrada_salida/TP_Entrada_Salida/03_ES_Py_camioneros.py
+++ 00-Unidades/01_entrada_salida/TP_Entrada_Salida/03_ES_Py_camioneros.py
@@ -46,9 +46,8 @@ class App(customtkinter.CTk):
         self.btn_tiempo_llegada.grid(row=4, pady=10, padx=30, columnspan=2, sticky="nsew")
     
     def btn_cantidad_camiones_on_click(self):
-        cant_toneladas = float(self.txt_toneladas.get())
-        cant_camiones = cant_toneladas & 35
-        
+        cant_toneladas = int(self.txt_toneladas.get())
+        cant_camiones = cant_toneladas / 3.5
 
         alert ("Mensaje", "Se necesitan "+ str(cant_camiones) +" para transportar " + str(cant_toneladas)+" toneladas")
 
