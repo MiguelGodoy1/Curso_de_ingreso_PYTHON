26641da1942c3294899aa1885105cbaa8c8aafbf
MiguelGodoy1 <Miguel.godoy28@hotmail.com>
2024-02-14 21:01:03 -0300

    Match 05

--- 00-Unidades/03_match/Ejercicios_Match/Match_app_05.py
+++ 00-Unidades/03_match/Ejercicios_Match/Match_app_05.py
@@ -35,7 +35,15 @@ class App(customtkinter.CTk):
         
     
     def btn_informar_on_click(self):
-        pass
+        hora = int(self.txt_hora.get())
+        mensaje = ""
+
+        match(hora) :
+            case 7 |8 | 9 | 10 | 11 :
+                mensaje = "Es de mañana"
+
+        alert ("UTN", mensaje)
+
     
     
 if __name__ == "__main__":
