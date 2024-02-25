92050a2080e5d4d79077e9d27a29eac78a76ac5e
MiguelGodoy1 <Miguel.godoy28@hotmail.com>
2024-02-25 16:37:37 -0300

    While

--- 00-Unidades/04_while/TP_While/06_While_Elecciones_Paso.py
+++ 00-Unidades/04_while/TP_While/06_While_Elecciones_Paso.py
@@ -35,9 +35,52 @@ class App(customtkinter.CTk):
         self.btn_validar.grid(row=4, pady=20, columnspan=2, sticky="nsew")
 
     def btn_validar_on_click(self):
-        pass
+        bandera = True
+        seguir = True
+        contador = 0
+        suma_edades = 0
+        suma_votos= 0
+        while True:
+            nombre= prompt ("Datos", "Ingrese su nombre: ")
+            while nombre == None or nombre == "":
+                nombre = prompt ("Error", "Reingrese su nombre: ")
+            
+            edad = prompt("Datos", "Ingrese su edad: ")
+            while (edad == None or edad == "") or int(edad) < 25 :
+                edad =  prompt ("Error", "Reingrese su edad: ")
+
+            edad = int(edad)
+            contador +=1
+            suma_edades += edad
 
 
+            cant_votos = prompt ("Datos", "Ingrese la cantidad de votos: ")
+            while cant_votos == None or cant_votos == 0 :
+                cant_votos = prompt ("Error", "Reingrese la cantidad de votos")
+            cant_votos = int(cant_votos)
+            suma_votos += cant_votos
+
+            if bandera == True or cant_votos > cant_votos_mayor:
+                cant_votos_mayor = cant_votos
+                nombre_mas_votos = nombre
+
+            if bandera == True or cant_votos < cant_votos_menor:
+                cant_votos_menor = cant_votos
+                
+                nombre_menos_votado = nombre
+            bandera = False
+
+            seguir = question ("Pregunta", "¿Desea seguir?")
+
+            promedio = suma_edades / contador
+            if seguir == False or seguir == None:
+                break    
+        
+        alert ("Mensaje", f"El candidato con mayor votos es {nombre_mas_votos}"
+            f"\nEl candidato {nombre_menos_votado}, tiene menos votos"
+            f"\nEl promedio de edad de los candidatos es {promedio}"
+            f"\nLa cantidad de votos emitidos fue {suma_votos}")
+        
 if __name__ == "__main__":
     app = App()
     app.geometry("300x300")
