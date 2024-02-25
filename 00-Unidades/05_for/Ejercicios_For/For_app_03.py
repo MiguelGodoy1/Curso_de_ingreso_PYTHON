2ef65e742a2893cb33eb0c58279f80068faa259e
MiguelGodoy1 <Miguel.godoy28@hotmail.com>
2024-02-25 16:54:02 -0300

    for 03

--- 00-Unidades/05_for/Ejercicios_For/For_app_03.py
+++ 00-Unidades/05_for/Ejercicios_For/For_app_03.py
@@ -32,7 +32,10 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        repetir = self.txt_repetir.get()
+        repetir = int (repetir)
+        for i in range (0, repetir):
+            alert ("Mensaje", "Hola UTN FRA")
 
         
     
